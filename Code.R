dbf38ca2d787fad1ccabc8d3cbdd6b5546ae7c1b
Christine-Z <zhaokejun325@gmail.com>
2022-04-02 18:12:35 +0200

    add plot

--- Code.R
+++ Code.R
@@ -1,8 +1,4 @@
 ###############
-# GLM Group 1 #
-###############
-
-###############
 # Environment #
 ###############
 
@@ -28,18 +24,11 @@ table(data)
 
 # Scientific question: Does race help explain how many homicide victims a person knows?
 
-##############
-# Question 1 #
-##############
 
 # Fit a Poisson model
 model_pos <- glm(resp ~ race, family = poisson, data = data )
 sum_pos <- summary(model_pos)
 sum_pos
-
-##############
-# Question 2 #
-##############
 
 # customized risk ratio function
 glm.RR <- function(GLM.RESULT, digits = 2) {
@@ -66,29 +55,16 @@ glm.RR <- function(GLM.RESULT, digits = 2) {
 beta1 <- model_pos$coefficients[2] # according to the slides, beta1 is relative risk
 glm.RR(model_pos, 5)
 
-##############
-# Question 3 #
-##############
-
 # Calculate the ratio of the means of the response for each race (mean response for black/mean response for white).
 mu_black <- mean(data$resp[data$race == 'black'])
 mu_white <- mean(data$resp[data$race == 'white'])
 ratio <- mu_black / mu_white
-
-##############
-# Question 4 #
-##############
 
 # Calculate the predictions of the models for each race
 newdata <- data.frame(race = c('black','white'))
 predata <- newdata
 predata$resp <- exp(predict(model_pos,newdata))
 predata
-
-
-##############
-# Question 5 #
-##############
 
 # Analyze the GOF of the model
 
@@ -122,33 +98,10 @@ root.pos <- rootogram(model_pos,ylab='Root Square of Frequency',main='Poisson')
 testUniformity(sim_model_pos)
 testDispersion(sim_model_pos)
 
-##############
-# Question 6 #
-##############
-
 # Fit a negative binomial model and get estimated model based variances (per race) for the counts.
 model_nb <- glm.nb(resp ~ race, data = data)
 sum_nb <- summary(model_nb)
 sum_nb
-
-# Compare them with the observed variances.
-# obs
-obs_var_b <- var(data[which(data$race == 'black'),1])
-obs_var_w <- var(data[which(data$race == 'white'),1])
-# model
-beta0 <- model_nb$coefficients[1]
-beta1 <- model_nb$coefficients[2]
-theta <- model_nb$theta
-md_var_b <- exp(beta0) + (1/theta)*exp(beta0)^2
-md_var_w <- exp(beta0 + beta1) + (1/theta)*exp(beta0 + beta1)^2
-
-comparison <- round(cbind(rbind(obs_var_b,obs_var_w),rbind(md_var_b,md_var_w),rbind(mu_black,mu_white)), 3)
-colnames(comparison) <- c("observed","neg.bin_model_based","poisson")
-comparison
-
-##############
-# Question 7 #
-##############
 
 # Fit a Quasi-likelihood model.
 model_qs <- glm(resp ~ race, data = data, family = quasipoisson)
@@ -159,7 +112,33 @@ sqrt(sum_qs$dispersion) - 1 # standard error
 # scaled deviance
 D_star <- model_qs$deviance / sum_qs$dispersion
 D_star
-##############
-# Discussion #
-##############
+
+# Compare them with the observed variances.
+# obs
+obs_var_b <- var(data[which(data$race == 'black'),1])
+obs_var_w <- var(data[which(data$race == 'white'),1])
+# model
+beta0 <- model_nb$coefficients[1]
+beta1 <- model_nb$coefficients[2]
+theta <- model_nb$theta
+nb_var_b <- exp(beta0) + (1/theta)*exp(beta0)^2
+nb_var_w <- exp(beta0 + beta1) + (1/theta)*exp(beta0 + beta1)^2
+
+qs_var_b <- sum_qs$dispersion * (-model_qs$coefficients[1])
+qs_var_w <- sum_qs$dispersion * (-model_qs$coefficients[2])
+
+comparison <- round(cbind(rbind(obs_var_b,obs_var_w),rbind(nb_var_b,nb_var_w),rbind(mu_black,mu_white),rbind(qs_var_b,qs_var_w)), 3)
+colnames(comparison) <- c("observed","neg.bin_model_based","poisson","quasi-possion")
+comparison
+
+## Plotting variance function
+xb<-predict(model_nb)
+g<-cut(xb, breaks=-2:0)
+m<-tapply(data$resp,g,mean)
+v<-tapply(data$resp,g,var)
+plot(m,v,xlab="Mean",ylab="Variance",main="Mean-Variance Relationship", xlim = c(0,1), ylim = c(0,2))
+x<-seq(0,1,0.02)
+lines(x,x*summary(model_qs)$dispersion,lty="dashed")
+lines(x,x*(1+x/model_nb$theta),col="red")
+legend("topleft",lty=c("dashed","solid"),legend=c("Q. Poisson","Neg. Binom."),col=c("black","red"),inset=0.05)
 
